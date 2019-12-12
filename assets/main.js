5a373ceac9df224a182bbad13d6caa798436a3f3
Rui Fonseca <rui.fonseca@yarilabs.com>
2019-12-12 14:51:56 +0000

    auto-resize after render

--- assets/main.js
+++ assets/main.js
@@ -13,13 +13,15 @@ $(function() {
   );
 });
 
-function showUserInfo(info, admin_url) {
-  // https://pactcoffee.zendesk.com/agent/tickets/361430?zat=true
+function showUserInfo(client, info, admin_url) {
   var source = $("#sidebar-template").html();
   var template = Handlebars.compile(source);
   info.admin_url = admin_url;
   var html = template(info);
   $("#content").html(html);
+
+  var height = $("html").height();
+  client.invoke('resize', { width: '100%', height: height });
 }
 
 function getUserInfo(client, metadata, user_id) {
@@ -31,49 +33,13 @@ function getUserInfo(client, metadata, user_id) {
 
   client.request(settings).then(
     function(data) {
-      showUserInfo(data, metadata.settings.admin_url);
+      showUserInfo(client, data, metadata.settings.admin_url);
     },
     function(response) {
-      $("#content").html("<p>Sorry, we couldn't find a user with this email</p>");
+      $("#content").html("<p class=\"not-found\">Sorry, we couldn't find a user with this email</p>");
+      var height = $("html").height();
+      client.invoke('resize', { width: '100%', height: height });
     }
   );
 }
 
-/*
-(function() {
-
-  return {
-    events: {
-      'app.activated':'initialize',
-      'findUser.fail' : 'this.fail',
-    },
-
-    requests: {
-      findUser: function(userId) {
-        return {
-          url: this.setting('api_url') + '/users/' + userId,
-          type: 'GET',
-          dataType: 'json',
-          cors: true,
-          xhrFields: {
-            withCredentials: true,
-          },
-        };
-      }
-    },
-
-    initialize: function() {
-      var userId  = this.ticket().requester().id();
-      this.ajax('findUser', userId)
-          .done(function(data) {
-            this.switchTo('admin_link', data);
-          });
-    },
-
-    fail: function(request) {
-      this.switchTo('error');
-    }
-  };
-
-}());
-*/
